4c68d82ac76524e9f50c8682b223007c8510d472
princekumar <princepk7878kumar@gmail.com>
2024-03-06 08:26:28 +0530

    done 03 01_program code

---
+++ Basics/03_AirthmaticOperator/02_program/02_program.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int x=5;
+//     x++;
+//     cout<<x<<endl;
+//     x--;
+//     cout<<x<<endl;
+//     ++x;
+//     cout<<x<<endl;
+//     --x;
+//     cout<<x<<endl;
+// }
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=5;
+    cout<<x++<<endl;
+    cout<<x<<endl;
+    cout<<x--<<endl;
+    cout<<x<<endl;
+    cout<<++x<<endl;
+    cout<<x<<endl;
+    cout<<--x<<endl;
+    cout<<x<<endl;
+}
