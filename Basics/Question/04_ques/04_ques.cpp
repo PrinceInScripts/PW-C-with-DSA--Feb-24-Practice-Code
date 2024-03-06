fdf4d67476197849200c31620463ba063ff1beed
princekumar <princepk7878kumar@gmail.com>
2024-03-06 10:39:45 +0530

    done 04_question"

---
+++ Basics/Question/04_ques/04_ques.cpp
@@ -0,0 +1,12 @@
+// Example : Calculating Simple Interest
+
+#include<iostream>
+using namespace std;
+int main(){
+   float p,r,t,si;
+   p=1000,
+   r=10,
+   t=2;
+   si=(p*r*t)/100;
+   cout<<si;
+}
