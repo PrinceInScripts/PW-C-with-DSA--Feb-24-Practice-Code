0d5e9b75f5a605374db966e5175769772e1dd551
princekumar <princepk7878kumar@gmail.com>
2024-03-14 10:17:26 +0530

    done 03 05 01_program

---
+++ 03_Loops/05_break & continue/01_program/01_program.cpp
@@ -0,0 +1,26 @@
+/*
+Ques : WAP to check if a number is prime or 
+not.
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    bool flag=true;
+
+    for(int i=2;i<=n/2;i++){
+        if(n%i==0){
+            flag=false;
+            break;
+        }
+    }
+
+    if(flag){
+        cout<<"Given number is prime number ";
+    }else {
+        cout<<"Given number is not a prime number ";
+    }
+}
