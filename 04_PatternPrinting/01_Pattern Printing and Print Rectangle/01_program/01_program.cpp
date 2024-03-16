2289daebb20e56fc4fb0291c4a5f8b39deb6c285
princekumar <princepk7878kumar@gmail.com>
2024-03-16 18:36:11 +0530

    done 04 01 01_program

--- 04_PatternPrinting/01_Pattern Printing and Print Rectangle/01_program/01_program.cpp
+++ 04_PatternPrinting/01_Pattern Printing and Print Rectangle/01_program/01_program.cpp
@@ -3,15 +3,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int m;
-    cout<<"enter a row no. : ";
-    cin>>m;
     int n;
-    cout<<"enter a column no. : ";
+    cout<<"enter a row no. : ";
     cin>>n;
+    int m;
+    cout<<"enter a column no. : ";
+    cin>>m;
     
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
             cout<<"* ";
         }
        
