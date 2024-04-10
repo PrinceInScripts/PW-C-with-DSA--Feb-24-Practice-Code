94283fa140f93cb0245bb5ed463b0163fb52c2ee
princekumar <princepk7878kumar@gmail.com>
2024-04-10 12:06:29 +0530

    done 10_String 01 03_program

--- 10_Strings/01_Introduction to strings/03_program/03_program.cpp
+++ 10_Strings/01_Introduction to strings/03_program/03_program.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-   string s="knight";
+//    string s="knight";
+   int n;
+   cout<<"Enter size f=of string : ";
+   cin>>n;
+
+   char s[n];
+   for(int i=0;i<n;i++){
+    cin>>s[i];
+   }
    int count=0;
    int i=0;
    while(s[i]!='\0'){
