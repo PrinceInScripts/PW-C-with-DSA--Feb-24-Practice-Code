604a4d2c7c861ed46b1178a7422475187bbeb4dc
princekumar <princepk7878kumar@gmail.com>
2024-04-24 20:14:19 +0530

    done

--- 10_Strings/02_BuiltInFunctions/07_program/07_program.cpp
+++ 10_Strings/02_BuiltInFunctions/07_program/07_program.cpp
@@ -7,7 +7,7 @@ int main(){
     string str="abcdef";
     //substr(idx)
     cout<<str<<endl;
-    cout<<str.substr(2)<<endl;//def
+    cout<<str.substr(2)<<endl;//cdef
     //substr(idx,len)
     cout<<str.substr(1,3)<<endl;//bcd
     // cout<<str.substr(2)<<endl;//def
