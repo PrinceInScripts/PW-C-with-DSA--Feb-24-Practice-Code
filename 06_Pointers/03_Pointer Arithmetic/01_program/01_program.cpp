cfe96ec1b378b2df15266308c2964bf78ab3d224
princekumar <princepk7878kumar@gmail.com>
2024-05-26 08:49:08 +0530

    done

--- 06_Pointers/03_Pointer Arithmetic/01_program/01_program.cpp
+++ 06_Pointers/03_Pointer Arithmetic/01_program/01_program.cpp
@@ -14,17 +14,17 @@ int main(){
 
     // ptr=ptr+1;
     // cout<<ptr<<endl;
-    // int x=7;
-    // int* ptr= &x;
-    // cout<<*ptr<<endl;
-
-    // ptr=ptr+1;
-    // cout<<*ptr<<endl;
     int x=7;
     int* ptr= &x;
     cout<<*ptr<<endl;
 
-    *ptr=*ptr+1;
-    (*ptr)++;
+    ptr=ptr+1;
     cout<<*ptr<<endl;
+    // int x=7;
+    // int* ptr= &x;
+    // cout<<*ptr<<endl;
+
+    // *ptr=*ptr+1;
+    // (*ptr)++;
+    // cout<<*ptr<<endl;
 }
