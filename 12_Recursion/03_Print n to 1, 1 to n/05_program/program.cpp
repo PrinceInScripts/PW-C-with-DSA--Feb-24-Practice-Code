c30a5d4a0930efe016229318b1d4c9b8a14f9246
princekumar <princepk7878kumar@gmail.com>
2024-05-28 20:10:16 +0530

    done

--- 12_Recursion/03_Print n to 1, 1 to n/05_program/program.cpp
+++ 12_Recursion/03_Print n to 1, 1 to n/05_program/program.cpp
@@ -4,6 +4,10 @@
 #include<iostream>
 using namespace std;
 int power(int a,int b){
+    if(a==0 && b==0){
+        cout<<"0 raised to power 0 is not defined";
+        return 0;
+    }
     if(a==0) return 0;
     int pow=1;
     for(int i=1;i<=b;i++){
@@ -12,5 +16,5 @@ int power(int a,int b){
     return pow;
 }
 int main(){
-    cout<<power(2,5);
+    cout<<power(0,0);
 }
