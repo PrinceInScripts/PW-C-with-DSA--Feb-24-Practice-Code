262ee9c769ab2b636aeaa2a65c31db734b2eb51d
princekumar <princepk7878kumar@gmail.com>
2024-05-28 19:38:55 +0530

    done

--- 12_Recursion/03_Print n to 1, 1 to n/02_program/program.cpp
+++ 12_Recursion/03_Print n to 1, 1 to n/02_program/program.cpp
@@ -6,10 +6,16 @@ void print(int i,int n){
     cout<<i<<endl;
     print(i+1,n);
 }
+void print2(int n){
+    if(n==0) return;
+    print2(n-1);
+    cout<<n<<endl;
+}
 int main(){
     int n;
     cin>>n;
     cout<<endl;
     print(1,n);
-    
+    cout<<endl;
+    print2(n);
 }
