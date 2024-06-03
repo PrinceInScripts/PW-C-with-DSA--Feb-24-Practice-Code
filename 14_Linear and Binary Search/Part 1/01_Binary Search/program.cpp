6a6c480d9e9b1cbce928d7a2c387b7830e1898f5
princekumar <princepk7878kumar@gmail.com>
2024-06-03 08:30:18 +0530

    done

--- 14_Linear and Binary Search/Part 1/01_Binary Search/program.cpp
+++ 14_Linear and Binary Search/Part 1/01_Binary Search/program.cpp
@@ -9,7 +9,8 @@ int search(vector<int>& nums,int target){
     int hi=nums.size()-1;
 
     while(lo<=hi){
-        int mid=(lo+hi)/2;
+        // int mid=(lo+hi)/2;
+        int mid=lo+(hi-lo)/2;
         if(nums[mid]==target) {
           return mid;
         }
