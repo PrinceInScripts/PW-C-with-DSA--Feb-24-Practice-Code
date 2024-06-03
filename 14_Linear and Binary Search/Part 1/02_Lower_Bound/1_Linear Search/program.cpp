087cbab929735192968e5e98ec9698fb0e2baefe
princekumar <princepk7878kumar@gmail.com>
2024-06-03 11:28:36 +0530

    done

--- 14_Linear and Binary Search/Part 1/02_Lower_Bound/1_Linear Search/program.cpp
+++ 14_Linear and Binary Search/Part 1/02_Lower_Bound/1_Linear Search/program.cpp
@@ -6,16 +6,25 @@ Ques: Given a sorted integer array and an integer
 #include<iostream>
 #include<vector>
 using namespace std;
-int lowerBound(int nums[],int target){
+int lowerBound1(int nums[],int target){
     int idx=0;
     while(nums[idx]<target){
         idx++;
     }
-    return idx-1;
+    return nums[idx-1];
+}
+int lowerBound2(int nums[],int target,int n){
+    for(int i=0;i<n;i++){
+        if(nums[i]>target){
+            return nums[i-1];
+        }
+    }
 }
 int main(){
     int nums[]={1,2,4,5,9,15,18,21,24};
+    int n=sizeof(nums)/sizeof(nums[0]);
     int target=20;
 
-    cout<<lowerBound(nums,target);
+    cout<<lowerBound1(nums,target)<<endl;
+    cout<<lowerBound2(nums,target,n);
 }
