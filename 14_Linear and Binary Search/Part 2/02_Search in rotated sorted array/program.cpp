cebc73d73ca0b0ad2e76f1d541b180adec106fca
princekumar <princepk7878kumar@gmail.com>
2024-06-04 20:12:21 +0530

    done

--- 14_Linear and Binary Search/Part 2/02_Search in rotated sorted array/program.cpp
+++ 14_Linear and Binary Search/Part 2/02_Search in rotated sorted array/program.cpp
@@ -7,31 +7,20 @@ Search in rotated sorted array
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> nums={6,8,20,28,33,1,2,3,5};
-    int target =5;
+    vector<int> nums={33,1,2,3,5};
+    int target =33;
 
     int lo=0;
     int hi=nums.size()-1;
 
     int pivot=-1;
 
-    while(lo<=hi){
+    while(lo<hi){
         int mid=lo+(hi-lo)/2;
-
-        if(mid==0) lo=mid+1;
-        else if(mid=nums.size()-1) hi=mid-1;
-       else if(nums[mid]<nums[mid-1] && nums[mid]<nums[mid+1]){
-            pivot=mid;
-            break;
-        }
-        else if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
-            pivot=mid+1;
-            break;
-        } else if(nums[mid]>nums[hi]) lo=mid+1;
-        else hi=mid-1;
-
-
+        if(nums[mid]>nums[hi]) lo=mid+1;
+        else hi=mid; 
     }
+    pivot=lo;
 
     if(target>= nums[0] && target<=nums[pivot-1]){
         lo=0;
@@ -61,6 +50,5 @@ int main(){
         else if(nums[mid]>target) hi=mid-1;
     }
 
-    // cout<<pivot;
 }
 }
