66693fe373553c59b0990ff552db7911b2e92bf4
princekumar <princepk7878kumar@gmail.com>
2024-06-21 20:37:48 +0530

    done

--- 16_Prefix Sum/02_Partition/program.cpp
+++ 16_Prefix Sum/02_Partition/program.cpp
@@ -8,15 +8,16 @@ using namespace std;
 int main(){
     int arr[]={1,2,3,4,5,5,10};
     int n=sizeof(arr)/sizeof(arr[0]);
-    bool flag=false;
+    int idx=-1;
     for(int i=1;i<n;i++){
         arr[i]+=arr[i-1];
     }
     for(int i=1;i<n;i++){
       if(2*arr[i]==arr[n-1]){
-        flag=true;
+        idx=i;
         break;
       }
     }
-    flag?cout<<"True":cout<<"False";
+    if(idx!=-1) cout<<"Yes It can be Partition after "<<idx;
+    else cout<<"It can't make partition";
 }
