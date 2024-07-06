d0ded8c73c750ac5dfa3bda6c77615a92a420257
princekumar <princepk7878kumar@gmail.com>
2024-07-06 11:27:08 +0530

    done

--- 20_OOPs/OOPs1/class and object/program.cpp
+++ 20_OOPs/OOPs1/class and object/program.cpp
@@ -12,7 +12,14 @@ int main(){
     Player hariram;
     hariram.score=80;
     hariram.health=60;
+    Player sayam;
+    sayam.score=80;
+    sayam.health=60;
 
     cout<<hariram.score<<endl;
     cout<<hariram.health<<endl;
+
+    Player player[10];
+    player[0]=hariram;
+    player[1]=sayam;
 }
