2b94208ea3ab541da6c6b00cfc1b40d1665214bb
princekumar <princepk7878kumar@gmail.com>
2024-07-12 08:30:13 +0530

    done

---
+++ 20_OOPs/OOPs2/5. Inheritence/01_program/program.cpp
@@ -0,0 +1,40 @@
+
+#include<iostream>
+using namespace std;
+
+class Vehicle{
+    public:
+    int tyreSize;
+    int engineSize;
+    int lights;
+    string companyName;
+
+    void showCompany(){
+        cout<<companyName<<endl;
+    }
+
+    Vehicle(){
+        cout<<"Vehicle Constructor is Call";
+    }
+};
+class Car:public Vehicle{
+    public:
+    int steeringSize;
+};
+class Bike:public Vehicle{
+    public:
+    int handleSize;
+    Bike(){
+        cout<<"Bike Constructor is Call";
+    }
+};
+
+int main(){
+    Bike honda;
+    honda.tyreSize=10;
+    honda.engineSize=40;
+    honda.lights=50;
+    honda.companyName="honda";
+    cout<<honda.engineSize;
+    honda.showCompany();
+}
